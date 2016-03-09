a1d1287ca7755bb9c88067c360f2399e0a88b56d
Pierre-Louis Dagues <dagues_p@yaka.epita.fr>
2016-03-09 10:31:03 +0100

    routes: add comments.
    * app/Http/Controllers/HomeController.php: Here.

--- web/app/Http/routes.php
+++ web/app/Http/routes.php
@@ -3,17 +3,40 @@
 Route::group(['prefix' => 'api'], function ()
 {
 
+  /*
+  |--------------------------------------------------------------------------
+  | API Routes
+  |--------------------------------------------------------------------------
+  | Most API Routes are protected by the API middleware which uses JWAuth
+  | to generate tokens and filter them.
+  |
+  */
   Route::group(['middleware' => 'api'], function (){
+
     Route::get('/', 'HomeController@GetBridge');
     Route::post('/light', 'LightsController@SetLights');
+
   });
 
+
+  // Token Generation routes.
   Route::post('/signup', 'UserController@Signup');
   Route::post('/signin', 'UserController@SignIn');
+
 });
 
+
+/*
+|--------------------------------------------------------------------------
+| Web App Routes
+|--------------------------------------------------------------------------
+| Routes for Web App to create views.
+| Protected by CSRF verification and Web MiddleWare.
+|
+*/
 Route::group(['middleware' => ['web']], function ()
 {
   //
   Route::get('/', 'HomeController@index');
+
 });
