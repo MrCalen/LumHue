f49b406871b08f8617beef1e67851794a064fe5b
Pierre-Louis Dagues <dagues_p@yaka.epita.fr>
2016-03-09 10:24:42 +0100

    mongo: update strict typing for MongoUtils.
    * app/Helpers/Mongo/Utils.php: Here.

--- web/app/Helpers/Mongo/Utils.php
+++ web/app/Helpers/Mongo/Utils.php
@@ -1,13 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Helpers\Mongo;
+use \MongoDB\Driver\Cursor;
+use \MongoDB\Driver\BSONDocument;
+use \MongoDB\BSON\Serializable;
 
 class Utils
 {
-  public static function MongoArray($bsonDocuments)
+  public static function MongoArray(Cursor $bsonDocuments) : array
   {
     $unserialized = [];
-    array_map(function ($element) use (&$unserialized) {
+    array_map(function (Serializable $element) use (&$unserialized) {
       $unserialized[] = $element->bsonSerialize();
     }, iterator_to_array($bsonDocuments));
     return $unserialized;
