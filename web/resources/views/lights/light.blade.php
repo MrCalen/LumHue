54a7e2faaccd2237ceed7c5abfbb5a8d2f278ea9
Corentin Brauge <brauge_c@epita.fr>
2016-03-23 12:56:09 +0100

    lights: separate js from html

--- web/resources/views/lights/light.blade.php
+++ web/resources/views/lights/light.blade.php
@@ -1,4 +1,10 @@
 @extends('templates/basic_nav')
+
+@section('javascript')
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+    <script src="{{ URL::asset('/js/lights.js') }}"></script>
+@endsection
+
 @section('content')
     <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
     <div ng-app="light" ng-controller="lightControl">
@@ -8,24 +14,6 @@
             <span class="row">Reachable: {$light.state.reachable$}</span>
             <span class="row">On: {$light.state.on$}</span>
         </div>
-        <script>
-            var app = angular.module('light', [], function($interpolateProvider) {
-                $interpolateProvider.startSymbol('{$').
-                endSymbol('$}')});
-            app.controller('lightControl', function($scope, $http) {
-                    $http.get("{{ URL::to('/')}}/api/bridge", {params: {"access_token": '{{$token}}'}})
-                            .success(function (data, status) {
-                                console.log("success");
-                                console.log(data[0].status.lights[1]);
-                                $scope.lights = data[0].status.lights;
-                                $scope.light = data[0].status.lights[1];
-                                $scope.name = $scope.light.name;
-                            })
-                            .error(function (data, status) {
-                        console.log("error");
-                    });
-            });
-        </script>
     </div>
 
 @endsection
