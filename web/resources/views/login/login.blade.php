abc488218baa528832ed4fa6736123888414e989
Maurer Hugo <maurer_h@epita.fr>
2016-03-27 18:05:01 +0200

    css: login and signup modified

--- web/resources/views/login/login.blade.php
+++ web/resources/views/login/login.blade.php
@@ -1,10 +1,12 @@
 @extends('templates/basic_nav')
 @section('content')
-  <center>
-    {{ Form::open(array('url' => 'login')) }}
-    {{ Form::text('email', Input::old('email'), array('placeholder' => 'User ID', 'class' => 'form-control login-field')) }}
-    {{ Form::password('password', array('placeholder' => 'Password', 'class' => 'form-control login-field')) }}
-    {{ Form::submit('Log in', array('class' => 'btn btn-primary btn-lg btn-block')) }}
-    {{ Form::close() }}
-  </center>
+  <div class="center-block">
+    <center>
+      {{ Form::open(array('url' => 'login')) }}
+      {{ Form::text('email', Input::old('email'), array('placeholder' => 'User ID', 'class' => 'form-control login-field')) }}
+      {{ Form::password('password', array('placeholder' => 'Password', 'class' => 'form-control login-field')) }}
+      {{ Form::submit('Log in', array('class' => 'btn btn-1 btn-1e')) }}
+      {{ Form::close() }}
+    </center>
+  </div>
 @endsection
--- web/resources/views/signup/signup.blade.php
+++ web/resources/views/signup/signup.blade.php
@@ -1,11 +1,13 @@
 @extends('templates/basic_nav')
 @section('content')
+  <div class="center-block">
   <center>
     {{ Form::open(array('url' => 'signup')) }}
     {{ Form::text('name', Input::old('name'), array('placeholder' => 'User name', 'class' => 'form-control login-field'))}}
     {{ Form::text('email', Input::old('email'), array('placeholder' => 'Email', 'class' => 'form-control login-field')) }}
     {{ Form::password('password', array('placeholder' => 'Password', 'class' => 'form-control login-field')) }}
-    {{ Form::submit('Sign up', array('class' => 'btn btn-primary btn-lg btn-block')) }}
+    {{ Form::submit('Sign up', array('class' => 'btn btn-1 btn-1e')) }}
     {{ Form::close() }}
   </center>
+</div>
 @endsection
