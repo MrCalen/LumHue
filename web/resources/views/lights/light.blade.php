a019563d7a41d609b57a12a344e58213cc60f572
Corentin Brauge <brauge_c@epita.fr>
2016-03-27 17:47:50 +0200

    lights: new style for display, including css/img

--- web/resources/views/lights/light.blade.php
+++ web/resources/views/lights/light.blade.php
@@ -3,8 +3,11 @@
 @section('ngApp')ng-app="light"@endsection
 @section('ngController')ng-controller="LightController"@endsection
 
+@section('css')
+    <link rel="stylesheet" href="{{ URL::asset('css/light/light.css') }}">
+@endsection
+
 @section('javascript')
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
     <script src="{{ elixir('js/app.js') }}"></script>
     <script>
       var token = '{{ $token }}';
@@ -14,13 +17,14 @@
 @endsection
 
 @section('content')
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+
     <div>
-        <div class="col-md-12">Lights</div>
-        <div class="col-md-4" ng-repeat="light in lights">
-            <span class="row">Name: {$light.name$}</span>
-            <span class="row">Reachable: {$light.state.reachable$}</span>
-            <span class="row">On: {$light.state.on$}</span>
+        <div class="row text-center light_info">Lights</div>
+        <div class="col-md-12" ng-repeat="light in lights">
+            <div class="row light_info_row">
+                <img id="lamp_reachable_{$light.state.reachable$}">
+                <span>{$light.name$}</span>
+            </div>
         </div>
     </div>
 
