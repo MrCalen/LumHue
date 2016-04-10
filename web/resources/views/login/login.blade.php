921b24ebd8b44e1e83e7c43f68f527d085f5acf4
Maurer Hugo <maurer_h@epita.fr>
2016-04-10 13:38:01 +0200

    webapp: login centered

--- web/resources/views/login/login.blade.php
+++ web/resources/views/login/login.blade.php
@@ -6,21 +6,25 @@
 @section('content')
   <div class="container-fluid">
     <div class="col-lg-3 center-block">
-      {{ Form::open(array('url' => 'login', 'ng-if' => '!open')) }}
+      <center>
+        {{ Form::open(array('url' => 'login', 'ng-if' => '!open')) }}
 
-      {{ Form::text('email', Input::old('email'), array('placeholder' => 'Your Email', 'class' => 'form-control login-field')) }}
-      @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
-
-      {{ Form::password('password', array('placeholder' => 'Password', 'class' => 'form-control login-field')) }}
-      @if ($errors->has('password')) <p class="help-block">{{ $errors->first('password') }}</p> @endif
-
-      {{ Form::submit('Log in', array('class' => 'btn btn-1 btn-1e')) }}
-      {{ Form::close() }}
-      <a class="forgot-pwd" ng-click="open = !open"><b ng-bind="!open ? 'Forgot Password ?' : 'Back'">Forgot password ? </b></a>
-        {{ Form::open(array('url' => 'signup/forgot', 'ng-if' => 'open')) }}
         {{ Form::text('email', Input::old('email'), array('placeholder' => 'Your Email', 'class' => 'form-control login-field')) }}
-        {{ Form::submit('Reset Password', array('class' => 'btn btn-1 btn-1e')) }}
+        @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
+
+        {{ Form::password('password', array('placeholder' => 'Password', 'class' => 'form-control login-field')) }}
+        @if ($errors->has('password')) <p class="help-block">{{ $errors->first('password') }}</p> @endif
+
+        {{ Form::submit('Log in', array('class' => 'btn btn-1 btn-1e')) }}
         {{ Form::close() }}
+      </center>
+        <div>
+            <a class="forgot-pwd" ng-click="open = !open"><b ng-bind="!open ? 'Forgot Password ?' : 'Back'">Forgot password ? </b></a>
+            {{ Form::open(array('url' => 'forgot', 'ng-if' => 'open')) }}
+            {{ Form::text('email', Input::old('email'), array('placeholder' => 'Your Email', 'class' => 'form-control login-field')) }}
+            {{ Form::submit('Reset Password', array('class' => 'btn btn-1 btn-1e')) }}
+            {{ Form::close() }}
+        </div>
     </div>
 </div>
 @endsection
